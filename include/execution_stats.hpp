e82b7e7fcd6b8ed678ebedf5602a5a341b6d9163
Aaron Suydam <aaron.suydam@ufl.edu>
2024-02-21 00:12:42 -0500

    Implement test_exec_time

--- include/execution_stats.hpp
+++ include/execution_stats.hpp
@@ -1,3 +1,25 @@
 #include <chrono>
 #include <iostream>
+#include <functional>
+using namespace std;
+
+
+
+/**
+ * @brief Determines the execution time of a function in nanocseconds.
+ * 
+ * @tparam ret_type The return type of the function.
+ * @tparam Argument_t The argument types of the function.
+ * @param to_benchmark The function to benchmark.
+ * @param args The arguments to pass to the function.
+ * @return The execution time of the function in nanoseconds.
+ */
+template <typename ret_type, typename... Argument_t>
+chrono::nanoseconds test_exec_time(std::function<ret_type> to_benchmark, Argument_t... args)
+{
+    auto start = chrono::high_resolution_clock::now();
+    to_benchmark(args...);
+    auto end = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::nanoseconds>(end - start);
+}
 
