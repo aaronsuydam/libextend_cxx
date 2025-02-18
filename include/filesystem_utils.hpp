863ed8600e99350a6a4821d15121428330540fd3
Aaron Suydam <aaron.suydam@ufl.edu>
2025-02-18 14:16:56 -0500

    Make fs open throw

--- include/filesystem_utils.hpp
+++ include/filesystem_utils.hpp
@@ -2,6 +2,8 @@
 #include <filesystem>
 #include <optional>
 #include <iostream>
+#include <errno.h>
+#include <cstring>
 using namespace std;
 
 namespace libex {
@@ -18,7 +20,21 @@ namespace libex {
         {
             try
             {
+                #if DEBUG 
+                string path_string = to_open.string();
+                std::cout << "Current directory is: " << std::filesystem::current_path() << endl;
+                ifstream to_return;
+                to_return.open(path_string);
+                #else
                 ifstream to_return(to_open.string());
+                #endif
+
+                auto success = to_return.good();
+                if(!success)
+                {
+                    std::cout << "File open failed, error is: " << strerror(errno) << endl;
+                    throw std::runtime_error("");
+                }
                 return to_return;
             }
             catch(const std::exception& e)
